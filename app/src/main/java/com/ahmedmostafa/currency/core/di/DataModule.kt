0325564e49e53010008f8b5ded9f477d42bb90ac
Ahmed Mostafa <ahmed.mostfa.ghorab@gmail.com>
2024-12-20 19:48:09 +0200

    Dependency Injection: Integrate Hilt Modules for dependency injection

---
+++ app/src/main/java/com/ahmedmostafa/currency/core/di/DataModule.kt
@@ -0,0 +1,43 @@
+package com.ahmedmostafa.currency.core.di
+
+import com.ahmedmostafa.currency.data.api.FixerApi
+import com.ahmedmostafa.currency.data.repositiry.CurrencyRepositoryImpl
+import com.ahmedmostafa.currency.domain.repository.CurrencyRepository
+import com.ahmedmostafa.currency.domain.usecase.GetCurrenciesUseCase
+import com.ahmedmostafa.currency.domain.usecase.GetExchangeRateUseCase
+import com.ahmedmostafa.currency.domain.usecase.GetHistoricalRatesUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataModule {
+
+    @Provides
+    @Singleton
+    fun provideCurrencyRepository(api: FixerApi): CurrencyRepository {
+        return CurrencyRepositoryImpl(api)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetCurrenciesUseCase(repository: CurrencyRepository): GetCurrenciesUseCase {
+        return GetCurrenciesUseCase(repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetExchangeRateUseCase(repository: CurrencyRepository): GetExchangeRateUseCase {
+        return GetExchangeRateUseCase(repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetHistoricalRatesUseCase(repository: CurrencyRepository): GetHistoricalRatesUseCase {
+        return GetHistoricalRatesUseCase(repository)
+    }
+
+}
